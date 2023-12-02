1abcd291c5bef50d4e13e971f10cedea3d5aa31b
mararo-ops <danielmararo1@gmail.com>
2023-12-03 02:53:20 +0300

    addition of the pseudocode

---
+++ restuarant.py
@@ -0,0 +1,10 @@
+# Restaurant __init__()`
+# - Restaurants should be initialized with a name, as a string
+#- `Restaurant name()`
+# - returns the restaurant's name
+#- should not be able to change after the restaurant is created
+
+#`  Restaurant reviews()`
+ # -returns a list of all reviews for that restaurant
+#- `Restaurant customers()`
+ # -Returns a **unique** list of all customers who have reviewed a particular restaurant.

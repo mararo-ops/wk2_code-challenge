ef2794d7e7c36525336e843e127198a3fa49e749
mararo-ops <danielmararo1@gmail.com>
2023-12-03 02:55:44 +0300

    creating a class constant

--- restuarant.py
+++ restuarant.py
@@ -14,3 +14,20 @@ class Restuarant():
     def __init__(self,restuarant_name): #initialized the restuarant with a name
         self._restuarant_name=restuarant_name
         self.reviews=[] #stores the reviews associated with the restuarant 
+
+
+    @property
+    def name(self):
+        return self._restuarant_name
+    
+    def reviews(self): #returns a list of all the reviews 
+        return self.reviews
+   
+
+    def customers(self): #returns list of unique customers who reviewed the restuarant
+        unique_customers=[review.customer() for review in self.reviews] #review.customer is in the review class
+        return list(unique_customers)
+ 
+    def avg_restuarant_rating(self):
+        avg_rating=sum(review.rating() for review in self.reviews) / len(self.reviews)
+        return avg_rating

15b798aa301b418826b392fcf743c06ab470413d
mararo-ops <danielmararo1@gmail.com>
2023-12-03 02:58:50 +0300

    writing the actual code

--- review.py
+++ review.py
@@ -12,3 +12,28 @@
   # `Review restaurant()`
  # - returns the restaurant object for that given review
  # - Once a review is created, should not be able to change the restaurant
+
+
+class Review():
+    all_reviews=[] #variable to hold all the reviews
+    def __init__(self,first_name,restuarant_name,rating):
+        self.rating = rating
+        self.first_name=first_name
+        self.restuarant_name=restuarant_name
+        # super().__init__(first_name,restuarant_name) #using super to initialize the name from the parent class
+        self.all_reviews.append(self)
+
+    def rating(self):
+        return self.rating
+    
+    def customer(self):
+        return self.first_name
+    
+    def restuarant(self):
+        return self.restuarant_name
+    
+    @classmethod
+    def review_all(cls): #returning all the reviews 
+        return cls.all_reviews
+   
+   

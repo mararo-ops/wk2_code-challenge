27434b594640ad1541c50d590a9a99319378f0d5
mararo-ops <danielmararo1@gmail.com>
2023-12-03 02:58:09 +0300

    writing the pseudocode.

---
+++ review.py
@@ -0,0 +1,14 @@
+#    Review __init__()`
+ # - Reviews should be initialized with a customer, restaurant, and a rating (a number)
+#-  `Review rating()`
+ # - returns the rating for a restaurant.
+#-  `Review all()`
+ # - returns all of the reviews
+
+
+ #- `Review customer()`
+ # - returns the customer object for that review
+  #- Once a review is created, should not be able to change the customer
+  # `Review restaurant()`
+ # - returns the restaurant object for that given review
+ # - Once a review is created, should not be able to change the restaurant

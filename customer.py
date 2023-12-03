1b4cc5392b5c579c4f551d7369181678fceba90a
mararo-ops <danielmararo1@gmail.com>
2023-12-03 03:01:05 +0300

    adding the class function and putting attributes in it

--- customer.py
+++ customer.py
@@ -1,1 +1,18 @@
 from review import Review
+
+
+class Customer:
+    customers=[] #declaring a variable to hold the customers
+    def __init__(self,first_name,last_name):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.customers.append(self) #appending each created instance to the list
+
+        self.reviews=[] # used to store each customers review
+    def given_name(self): #getter method used to retrieve the name
+        return self.first_name
+    def family_name(self):
+        return self.last_name
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    

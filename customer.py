6a0c61b223d6c766ef5c478dac9363921a5e6364
mararo-ops <danielmararo1@gmail.com>
2023-12-03 03:02:11 +0300

    adding the first class constatnt

--- customer.py
+++ customer.py
@@ -16,3 +16,20 @@ class Customer:
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
     
+    @classmethod
+    def all(cls): #used to refer to the class itself and returns  a list of all customer instances
+        return cls.customers
+    
+    def restuarants(self): #list of restuarants reviewed 
+        restuarants_reviewed=[review.restuarant() for review in self.reviews]
+        return list(restuarants_reviewed)
+    
+    def add_review(self,restuarant_name,rating):
+        new_review=Review(self,restuarant_name,rating) #creating a new instance of a review 
+        self.reviews.append(new_review) #adding the new review to the review list 
+
+    def num_reviews(self): # return the total number of reviews by checking the length
+        return len(self.reviews)
+
+   
+    

9d1ea69a879804fcf74e23041da4077e0925475a
mararo-ops <danielmararo1@gmail.com>
2023-12-03 03:09:43 +0300

    final commit

--- main.py
+++ main.py
@@ -30,3 +30,20 @@ print(review1.rating) #printing the rating
 print(Review.review_all()) #printing all the review instances 
 print(review1.customer().full_name())  # retrieving details from the customer class
 print(review1.restuarant().name) 
+
+
+#testing out adding the reviews 
+customer1.add_review(restuarant1,5)
+customer2.add_review(restuarant2,3)
+
+#printing to see if the reviews were added 
+print(restuarant1.reviews)
+
+#printing the reviews of an individual customer
+print(customer1.reviews) 
+
+#print the individual customers number of reviews he/she has made
+print(customer1.num_reviews())
+
+#the restuarant each customer has
+print(customer1.restuarants())  

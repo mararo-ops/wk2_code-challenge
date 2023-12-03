3008fee37c502813b5c3384257cf66341c35ad03
mararo-ops <danielmararo1@gmail.com>
2023-12-03 03:08:11 +0300

    output for restaurant

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ print(customer2.full_name())
 print(customer2.given_name())
 print(customer2.family_name())
 print(customer2.all()) #its a class method
+
+
+#output for restuarant
+restuarant1=Restuarant("Frangos")
+restuarant2=Restuarant("The Curve")
+
+print (restuarant1.name)
+print (restuarant2.name)

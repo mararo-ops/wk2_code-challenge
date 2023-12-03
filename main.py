2813242fe294951ab1ec14c49d763e539b9bf497
mararo-ops <danielmararo1@gmail.com>
2023-12-03 03:07:30 +0300

    output for customers

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 from customer import Customer
 from restuarant import Restuarant
 from review import Review
+
+
+#output for customers
+customer1 = Customer("Judy", "sigilai")
+customer2 = Customer("Brian", "Kip")
+
+print(customer1.full_name())  
+print(customer2.full_name())
+print(customer2.given_name())
+print(customer2.family_name())
+print(customer2.all()) #its a class method
